bd503ce9f4f1018d1817586517648f06484e07e9
Grigor-MF <grigor.ferreira@sptech.school>
2025-11-16 16:38:24 -0300

    Fixbugs: classe TratamentoRepository

--- careplus/careplus/src/main/java/com/example/careplus/repository/TratamentoRepository.java
+++ careplus/careplus/src/main/java/com/example/careplus/repository/TratamentoRepository.java
@@ -11,10 +11,10 @@ import java.util.List;
 
 public interface TratamentoRepository extends JpaRepository<Tratamento, Long> {
 
-    /* Como prontuario nao existe vou deixar comentado para buscar a quantidades de tratamentos já feito;
+
     @Query("SELECT COUNT(t) FROM Prontuario p LEFT JOIN p.tratamentos t WHERE p.id = :idProntuario")
     Long buscarQuantidadeDeTratamentosPorId( Long idProntuario);
-     */
+
     List<Tratamento> findByProntuario_Id(Long idProntuario);
 
     List<Tratamento> findByTipoDeTratamento(String nome);
